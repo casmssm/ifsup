07822dc70442fd4293d34a377278173efe470161
Carlos Martins <casmssm@hotmail.com>
2019-02-17 18:11:47 -0300

    Update

---
+++ Server/bin/teste.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FULL_PATH="$(realpath $0)"
+FULL_FOLDER="$(dirname $FULL_PATH)"
+
+source "$FULL_FOLDER/../ifsup.conf"
+
+echo "[$MYPORT]"
+echo "[$AUTH_TIMEOUT]"
+echo -n "auth|$MYIPv4|$MYPORT|$AUTH_TIMEOUT" | ncat -n -w$AUTH_TIMEOUT 10.0.0.11 8888 --send-only
+#mysql -B -N -u "'$ifsupSQLUSER'" -p"$ifsupSQLPASS" -D "$ifsupSQLBASE" -h $ifsupSQLADDR -e "INSERT INTO usuario (usuario, senha, quota, idgrupo, admin) VALUES ('SS','bankai','5','1',false)"
+
+#CLIENTIP="200.131.80.3"
+
+#sed -i -n "/$CLIENTIP/!p" waitlist
+
+#while [[ "$(cat waitlist | grep "2001")" != "" ]];do
+#sleep 1
+#done
